be71a37807d2a70f924277ce362593329a375193
Astakhov Aleksandr <astakhov.am@phystech.edu>
2025-12-17 04:54:40 +0300

    update

--- model/GRAND.py
+++ model/GRAND.py
@@ -1,90 +1,59 @@
 import torch
 import torch.nn as nn
-from torchdiffeq import odeint
+from torchdiffeq import odeint_adjoint as odeint
+import torch_geometric.nn as gnn
+import torch_geometric as torchg
 
 
-class GRANDBlock(nn.Module):
-    def __init__(self, num_heads, num_chanels, featurs_dim, out_dim, rho=0.7):
-        super().__init__()
+class GRANDDiffusionODEFunc(gnn.MessagePassing):
+    def __init__(self, vert_dim, model_dim, num_heads):
+        super().__init__(flow="target_to_source")
 
+        self.model_dim = model_dim
+        self.vert_dim = vert_dim
         self.num_heads = num_heads
-        self.num_chanels = num_chanels
-        self.featurs_dim = featurs_dim
-        self.out_dim = out_dim
-        self.rho = rho
 
-        self.register_buffer("eye", torch.eye(self.num_chanels))
+        self.k = nn.Linear(vert_dim, num_heads * model_dim)
+        self.q = nn.Linear(vert_dim, num_heads * model_dim)
 
-        self.k = nn.Linear(self.featurs_dim, self.out_dim * self.num_heads)
-        self.q = nn.Linear(self.featurs_dim, self.out_dim * self.num_heads)
-        self.v = nn.Linear(self.featurs_dim, self.out_dim)
+    def update(self, aggr_out):
+        return aggr_out
 
-    def forward(
-        self,
-        x,
-    ):
+    def message(self, x_j, x_i, index):
+        Q_i = self.q(x_i).view(-1, self.num_heads, self.model_dim)
+        K_j = self.k(x_j).view(-1, self.num_heads, self.model_dim)
 
-        k = self.k(x).reshape(-1, self.num_heads, self.num_chanels, self.out_dim)
-        q = self.q(x).reshape(-1, self.num_heads, self.num_chanels, self.out_dim)
-        v = self.v(x)
+        scores = (Q_i * K_j).sum(dim=-1) / torch.sqrt(self.model_dim)
+        scores = scores.mean(dim=1)
+        alpha = torchg.utils.softmax(scores, index)
+        return (x_j - x_i) * alpha.unsqueeze(1)
 
-        att_scores = torch.einsum("bhcf,bhdf->bhcd", q, k) / (self.out_dim**0.5)
-
-        att_map = torch.softmax(
-            (torch.softmax(att_scores, dim=-1) > self.rho).long() * att_scores, dim=-1
-        ).mean(dim=1)
-
-        out = torch.einsum(
-            "bcd,bdf->bcf",
-            att_map - self.eye,
-            v,
-        )
-
-        return out
+    def forward(self, edge_index, x):
+        return self.propagate(edge_index, x=x)
 
 
 class ODEFunc(nn.Module):
-    def __init__(self, func, **kwargs):
+    def __init__(self, func, edge_index):
         super().__init__()
-
         self.func = func
-        self.kwargs = kwargs
+        self.edge_index = edge_index
 
     def forward(self, t, x):
-        return self.func(x)
-
-
-class GRAND(nn.Module):
-    def __init__(self, num_heads, num_chanels, featurs_dim, out_dim):
-        super().__init__()
-
-        self.grand_encoder = ODEFunc(
-            GRANDBlock(num_heads, num_chanels, featurs_dim, out_dim)
-        )
-
-    def forward(self, x, t):
-        z = odeint(self.grand_encoder, x, t, method="rk4")[-1]
-
-        return z
+        return self.func(self.edge_index, x)
 
 
 class GRANDAutoencoder(nn.Module):
-    def __init__(self, num_heads, num_chanels, featurs_dim, latent_dim):
+    def __init__(self, edge_index, vert_dim, model_dim, num_heads):
         super().__init__()
+        self.encoder = GRANDDiffusionODEFunc(vert_dim, model_dim, num_heads)
+        self.decoder = GRANDDiffusionODEFunc(vert_dim, model_dim, num_heads)
 
-        self.encoder = ODEFunc(
-            GRANDBlock(num_heads, num_chanels, featurs_dim, latent_dim)
-        )
-        self.decoder = ODEFunc(
-            GRANDBlock(num_heads, num_chanels, latent_dim, featurs_dim)
-        )
-
-        self.enc_linear = nn.Linear(num_chanels, 3)
+        self.encoder_ode = ODEFunc(self.encoder, edge_index)
+        self.decoder_ode = ODEFunc(self.decoder, edge_index)
 
     def forward(self, x, t):
-        z = odeint(self.encoder, x, t, method="rk4")[-1]
-        z_lin = self.enc_linear(z.permute(0, 2, 1)).permute(0, 2, 1)
 
-        x_hat = odeint(self.decoder, z, t, method="rk4")[-1]
+        z = odeint(self.encoder_ode, x, t)[-1]
+        x_hat = odeint(self.decoder_ode, z, t)[-1]
 
-        return z_lin, x_hat
+        return x_hat, z
